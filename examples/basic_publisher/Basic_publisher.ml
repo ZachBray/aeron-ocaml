c88b539a9fb623a9f8b45568fe4f37424ee56a8d
Zach Bray <zachbray@googlemail.com>
2021-12-20 00:04:35 +0000

    Partially port basic_publisher sample.
    This commit contains an example that acts much like the basic_publisher
    example in the main Aeron project; however, rather than publishing a
    "hello world" string, it publishes a message with three 32-bit integer
    values. This deviation is because the `Unsafe_buffer` has only limited
    functionality, at the time of writing.
    
    It may be used in conjunction with the basic_subscriber samples in the
    main Aeron project and an external media driver.
    
    Please note that I have not configured static linking. Therefore, to run
    the example, one must configure `LD_LIBRARY_PATH` so that the linker can
    find the necessary shared libraries. For example:
    
    ```
    LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/zach/src/personal/aeron-ocaml/_build/default/lib ./_build/default/examples/basic_publisher/basic_publisher.exe
    ```

---
+++ examples/basic_publisher/Basic_publisher.ml
@@ -0,0 +1,44 @@
+module System = Sys
+open Core
+open Aeron_ocaml
+open Shapeshifter
+
+let offer_loop publication =
+  let is_running = ref true in
+  let terminate _ = is_running := false in
+  System.set_signal System.sigint (System.Signal_handle terminate) ;
+  let buffer = Unsafe_buffer.acquire ~byte_length:64l in
+  Unsafe_buffer.set_i32 ~offset:0l ~value:42l buffer ;
+  Unsafe_buffer.set_i32 ~offset:4l ~value:43l buffer ;
+  Unsafe_buffer.set_i32 ~offset:8l ~value:44l buffer ;
+  let rec offer_loop_aux () =
+    let module Publication = Aeron_client.Publication in
+    if !is_running then (
+      let result =
+        Publication.offer ~buffer ~offset:0l ~length:12l publication
+      in
+      (* TODO call idle strategy here *)
+      ignore result ; offer_loop_aux () )
+    else ()
+  in
+  offer_loop_aux () ;
+  Unsafe_buffer.release buffer
+
+let run_publisher () =
+  let open Result.Let_syntax in
+  let module Context = Aeron_client.Context in
+  let ctx =
+    Context.default |> Context.with_aeron_dir "/dev/shm/example-driver"
+  in
+  let%bind client = Aeron_client.start ctx in
+  let%map publication =
+    Aeron_client.add_exclusive_publication
+      ~channel_uri:"aeron:udp?endpoint=localhost:20121" ~stream_id:1001l
+      client
+  in
+  offer_loop publication ; Aeron_client.close client
+
+let () =
+  match run_publisher () with
+  | Ok _ -> ()
+  | Error msg -> print_endline ("Error: " ^ msg)
